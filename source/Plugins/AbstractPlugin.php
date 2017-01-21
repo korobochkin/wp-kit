f7f13fe05b789822c1d3845a58316d0b512ce02b
Kolya Korobochkin <me@korobochkin.com>
2017-01-21 19:57:23 +0300

    plugin interface methods

--- source/Plugins/AbstractPlugin.php
+++ source/Plugins/AbstractPlugin.php
@@ -4,23 +4,47 @@ namespace Korobochkin\WPKit\Plugins;
 abstract class AbstractPlugin implements PluginInterface {
 
 	/**
-	 * @var string A path for plugin bootstrap file.
+	 * @var string A path to plugin bootstrap file.
 	 */
 	protected $file;
 
 	/**
-	 * @return string
+	 * @inheritdoc
+	 */
+	public function __construct( $file ) {
+		$this->setFile($file);
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	abstract public function run();
+
+	/**
+	 * @inheritdoc
 	 */
 	public function getFile() {
 		return $this->file;
 	}
 
 	/**
-	 * @param string $file
+	 * @inheritdoc
 	 */
 	public function setFile( $file ) {
 		$this->file = $file;
 	}
 
+	/**
+	 * @inheritdoc
+	 */
+	public function getDir() {
+		return plugin_dir_path($this->getFile());
+	}
 
+	/**
+	 * @inheritdoc
+	 */
+	public function getUrl() {
+		return plugin_dir_url($this->getFile());
+	}
 }
--- source/Plugins/PluginInterface.php
+++ source/Plugins/PluginInterface.php
@@ -3,6 +3,37 @@ namespace Korobochkin\WPKit\Plugins;
 
 interface PluginInterface {
 
+	/**
+	 * PluginInterface constructor.
+	 *
+	 * Do not set any actions or hooks here.
+	 *
+	 * @param $file string Path to plugin bootstrap file.
+	 */
+	public function __construct($file);
+
+	/**
+	 * The main function which runs everything. Place your add_action() or other functions call here.
+	 */
+	public function run();
+
+	/**
+	 * @return string A path to plugin bootstrap file.
+	 */
 	public function getFile();
+
+	/**
+	 * @param $file string A path to plugin bootstrap file.
+	 */
 	public function setFile($file);
+
+	/**
+	 * @return string A path to plugin root folder (where your bootstrap file located).
+	 */
+	public function getDir();
+
+	/**
+	 * @return string An URL to plugin root folder (where you can place your assets folder for example).
+	 */
+	public function getUrl();
 }

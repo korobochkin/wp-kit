5084d78d14baa0f577a2fa27ae17a21d06afd00b
Kolya Korobochkin <me@korobochkin.com>
2017-01-27 01:05:58 +0300

    test

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -21,5 +21,6 @@ require_once $_tests_dir . '/includes/functions.php';
 }*/
 //tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';

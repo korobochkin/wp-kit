faee72632980a5f0e11e4b1b2b1bebdde22217d8
Kolya Korobochkin <me@korobochkin.com>
2017-01-27 01:32:24 +0300

    require plugin

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -16,10 +16,10 @@ require_once $_tests_dir . '/includes/functions.php';
 /**
  * Manually load the plugin being tested.
  */
-/*function _manually_load_plugin() {
+function _manually_load_plugin() {
 	require dirname( dirname( __FILE__ ) ) . '/plugin.php';
-}*/
-//tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 // Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';

e2358db1649a0a8ab08cbf1c368e41e3f1930826
Kolya Korobochkin <me@korobochkin.com>
2017-01-26 12:32:40 +0300

    update sanitizer

--- source/Sanitizers/BoolSanitizer.php
+++ source/Sanitizers/BoolSanitizer.php
@@ -4,6 +4,12 @@ namespace Korobochkin\WPKit\Sanitizers;
 class BoolSanitizer implements SanitizerInterface {
 
 	public static function sanitize($value) {
+		if(is_string($value)) {
+			if($value == '1')
+				return true;
+			elseif($value == '0')
+				return false;
+		}
 		return (bool)$value;
 	}
 }

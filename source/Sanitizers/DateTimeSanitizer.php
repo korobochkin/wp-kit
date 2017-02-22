082ea8b85cd341d84d42a165f71ffcf681b291e0
Kolya Korobochkin <me@korobochkin.com>
2017-02-23 00:41:01 +0300

    iso sanitizer

--- source/Sanitizers/DateTimeSanitizer.php
+++ source/Sanitizers/DateTimeSanitizer.php
@@ -5,10 +5,14 @@ class DateTimeSanitizer implements SanitizerInterface {
 
 	public static function sanitize($value) {
 		try {
-			return new \DateTime($value);
+			return \DateTime::createFromFormat(\DateTime::ISO8601, $value);
 		}
 		catch(\Exception $exception) {
 			return false;
 		}
+	}
+
+	public static function unSanitize(\DateTime $value) {
+		return $value->format(\DateTime::ISO8601);
 	}
 }

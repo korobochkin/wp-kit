80e6fe111ee495d9182c39edcca5da649b749924
Kolya Korobochkin <me@korobochkin.com>
2017-02-26 00:35:43 +0300

    theme

---
+++ source/Themes/ConstantsAbstractTheme.php
@@ -0,0 +1,11 @@
+<?php
+namespace Korobochkin\WPKit\Themes;
+
+use Korobochkin\WPKit\Traits;
+
+abstract class ConstantsAbstractTheme extends AbstractTheme {
+
+	use Traits\ConstVersionTrait;
+
+	use Traits\ConstNameTrait;
+}

a0d35c6692364c767b51e3e602887d8351eab4a4
Kolya Korobochkin <me@korobochkin.com>
2017-03-29 23:13:15 +0300

    describe aggregate node interface

---
+++ source/DataComponents/AggregateNodeInterface.php
@@ -0,0 +1,39 @@
+<?php
+namespace Korobochkin\WPKit\DataComponents;
+
+interface AggregateNodeInterface {
+
+	/**
+	 * Returns all children nodes list in array.
+	 *
+	 * @return array The nodes list.
+	 */
+	public function getNodes();
+
+	/**
+	 * Add single node to this instance.
+	 *
+	 * @param NodeInterface $node Node to add
+	 *
+	 * @return $this For chain calls.
+	 */
+	public function addNode(NodeInterface $node);
+
+	/**
+	 * Remove single node by name.
+	 *
+	 * @param $name string The node name.
+	 *
+	 * @return mixed ???
+	 */
+	public function removeNode($name);
+
+	/**
+	 * Get single node by name.
+	 *
+	 * @param $name string The node name.
+	 *
+	 * @return mixed ???
+	 */
+	public function getNode($name);
+}

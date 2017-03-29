fc0d9af633ea61329a91eba2a2f47ef0ad8c5801
Kolya Korobochkin <me@korobochkin.com>
2017-03-29 23:54:15 +0300

    doc

--- source/Options/Option.php
+++ source/Options/Option.php
@@ -1,6 +1,9 @@
 <?php
 namespace Korobochkin\WPKit\Options;
 
+/**
+ * This class can be used for dynamic options (from other plugins or for managing default WP options).
+ */
 class Option extends AbstractOption {
 
 	public function buildConstraint() {

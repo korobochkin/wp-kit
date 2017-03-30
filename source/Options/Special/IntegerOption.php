8d9130847e10f15619b6954ac4514b73c31dfe79
Kolya Korobochkin <me@korobochkin.com>
2017-03-31 00:12:37 +0300

    integer option

---
+++ source/Options/Special/IntegerOption.php
@@ -0,0 +1,25 @@
+<?php
+namespace Korobochkin\WPKit\Options\Special;
+
+use Korobochkin\WPKit\Options\AbstractOption;
+use Symfony\Component\Form\Extension\Core\DataTransformer\NumberToLocalizedStringTransformer;
+use Symfony\Component\Validator\Constraints;
+
+class IntegerOption extends AbstractOption {
+
+	/**
+	 * FloatOption constructor.
+	 */
+	public function __construct() {
+		$this->setDataTransformer(new NumberToLocalizedStringTransformer());
+	}
+
+	public function buildConstraint() {
+		return array(
+			new Constraints\NotBlank(),
+			new Constraints\Type(array(
+				'type' => 'integer',
+			)),
+		);
+	}
+}

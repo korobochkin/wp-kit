6e2bb3810a7271066ddef1c0fe3b24b6d455c058
Kolya Korobochkin <me@korobochkin.com>
2017-03-31 02:49:24 +0300

    datetime option

---
+++ source/Options/Special/DateTimeOption.php
@@ -0,0 +1,23 @@
+<?php
+namespace Korobochkin\WPKit\Options\Special;
+
+use Korobochkin\WPKit\Options\AbstractOption;
+use Symfony\Component\Form\Extension\Core\DataTransformer\DateTimeToStringTransformer;
+use Symfony\Component\Validator\Constraints;
+
+class DateTimeOption extends AbstractOption {
+
+	/**
+	 * FloatOption constructor.
+	 */
+	public function __construct() {
+		$this->setDataTransformer(new DateTimeToStringTransformer());
+	}
+
+	public function buildConstraint() {
+		return array(
+			new Constraints\NotBlank(),
+			new Constraints\DateTime(),
+		);
+	}
+}

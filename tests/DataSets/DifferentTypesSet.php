8c5a3ff30307ff6ceb960af7abbba0c6bea42a3e
Kolya Korobochkin <me@korobochkin.com>
2017-04-09 00:13:14 +0300

    mark test cases

--- tests/DataSets/DifferentTypesSet.php
+++ tests/DataSets/DifferentTypesSet.php
@@ -12,45 +12,45 @@ class DifferentTypesSet implements \Iterator {
 	 */
 	public function __construct() {
 		$values = array(
-			array(true),
-			array(false),
+			array(true), // 0
+			array(false), // 1
 
-			array(1234),
-			array(0),
-			array(-1234),
-			array(PHP_INT_MAX),
+			array(1234), // 2
+			array(0), // 3
+			array(-1234), // 4
+			array(PHP_INT_MAX), // 5
 			//array(PHP_INT_MIN, true),
 
-			array(1.234),
-			array(1.2e3),
-			array(7E-10),
-			array(-1.234),
-			array(-1.2e3),
-			array(-7E-10),
+			array(1.234), // 6
+			array(1.2e3), // 7
+			array(7E-10), // 8
+			array(-1.234), // 9
+			array(-1.2e3), // 10
+			array(-7E-10), // 11
 
-			array('1'),
-			array('VALUE'),
-			array('true'),
-			array('false'),
-			array(''),
-			array('0'),
+			array('1'), // 12
+			array('VALUE'), // 13
+			array('true'), // 14
+			array('false'), // 15
+			array(''), // 16
+			array('0'), // 17
 
-			array(array()),
-			array(array(1)),
-			array(array(1, 2)),
-			array(array('')),
-			array(array('1')),
-			array(array('0')),
+			array(array()), // 18
+			array(array(1)), // 19
+			array(array(1, 2)), // 20
+			array(array('')), // 21
+			array(array('1')), // 22
+			array(array('0')), // 23
 
-			array(new \stdClass()),
-			array(new \WP_Query()),
+			array(new \stdClass()), // 24
+			array(new \WP_Query()), // 25
 
-			array(NULL)
+			array(NULL), // 26
 		);
 
 		// Only for PHP 7
 		if(PHP_VERSION_ID >= 70000) {
-			$values[] = array(PHP_INT_MIN);
+			$values[] = array(PHP_INT_MIN); // 27
 		}
 
 		$this->variants = $values;

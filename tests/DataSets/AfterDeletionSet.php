e76857aadd041260a5e4024c71a072471be2cf8b
Kolya Korobochkin <me@korobochkin.com>
2017-04-09 13:24:01 +0300

    format

--- tests/DataSets/AfterDeletionSet.php
+++ tests/DataSets/AfterDeletionSet.php
@@ -5,40 +5,40 @@ class AfterDeletionSet extends AbstractDataSet {
 
 	public function __construct() {
 		$variants = array(
-			array(true, true), // 0
-			array(false, false), // 1
+			array(true,            true), // 0
+			array(false,           false), // 1
 
-			array(1234, true), // 2
-			array(0, true), // 3
-			array(-1234, true), // 4
-			array(PHP_INT_MAX, true), // 5
-			//array(PHP_INT_MIN, true),
+			array(1234,            true), // 2
+			array(0,               true), // 3
+			array(-1234,           true), // 4
+			array(PHP_INT_MAX,     true), // 5
+			//array(PHP_INT_MIN,   true),
 
-			array(1.234, true), // 6
-			array(1.2e3, true), // 7
-			array(7E-10, true), // 8
-			array(-1.234, true), // 9
-			array(-1.2e3, true), // 10
-			array(-7E-10, true), // 11
+			array(1.234,           true, ), // 6
+			array(1.2e3,           true), // 7
+			array(7E-10,           true), // 8
+			array(-1.234,          true), // 9
+			array(-1.2e3,          true), // 10
+			array(-7E-10,          true), // 11
 
-			array('1', true), // 12
-			array('VALUE', true), // 13
-			array('true', true), // 14
-			array('false', true), // 15
-			array('', true), // 16
-			array('0', true), // 17
+			array('1',             true), // 12
+			array('VALUE',         true), // 13
+			array('true',          true), // 14
+			array('false',         true), // 15
+			array('',              true), // 16
+			array('0',             true), // 17
 
-			array(array(), true), // 18
-			array(array(1), true), // 19
-			array(array(1, 2), true), // 20
-			array(array(''), true), // 21
-			array(array('1'), true), // 22
-			array(array('0'), true), // 23
+			array(array(),         true), // 18
+			array(array(1),        true), // 19
+			array(array(1, 2),     true), // 20
+			array(array(''),       true), // 21
+			array(array('1'),      true), // 22
+			array(array('0'),      true), // 23
 
 			array(new \stdClass(), true), // 24
 			array(new \WP_Query(), true), // 25
 
-			array(NULL, false), // 26
+			array(NULL,            false), // 26
 		);
 
 		// Only for PHP 7

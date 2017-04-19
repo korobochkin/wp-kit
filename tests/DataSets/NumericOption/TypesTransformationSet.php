dc219bd289b0174807b386e77613d0ccb6106d6c
Kolya Korobochkin <me@korobochkin.com>
2017-04-20 00:18:47 +0300

    relocate test cases

---
+++ tests/DataSets/NumericOption/TypesTransformationSet.php
@@ -0,0 +1,59 @@
+<?php
+namespace Korobochkin\WPKit\Tests\DataSets\NumericOption;
+
+use Korobochkin\WPKit\Tests\DataSets\AbstractDataSet;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+class TypesTransformationSet extends AbstractDataSet {
+
+	/**
+	 * TypesTransformationSet constructor.
+	 */
+	public function __construct() {
+		$variants = array(
+			array(true,        TransformationFailedException::class),
+			array(false,       TransformationFailedException::class),
+
+			array(1234,        1234.0),
+			array(0,           0.0),
+			array(-1234,       -1234.0),
+			//array(PHP_INT_MAX, TransformationFailedException::class), // this case throwing error but PHP 7 not catching it
+			//array(PHP_INT_MIN, true),
+
+			array(1.234,       1.234),
+			array(1.2e3,       1.2e3),
+			array(7E-10,       7E-10),
+			array(-1.234,      -1.234),
+			array(-1.2e3,      -1.2e3),
+			array(-7E-10,      -7E-10),
+
+			array('1',         1.0),
+			array('VALUE',     TransformationFailedException::class),
+			array('true',      TransformationFailedException::class),
+			array('false',     TransformationFailedException::class),
+			array('',          TransformationFailedException::class),
+			array('0',         0.0),
+
+			array(array(),     TransformationFailedException::class),
+			array(array(1),    TransformationFailedException::class),
+			array(array(1, 2), TransformationFailedException::class),
+			array(array(''),   TransformationFailedException::class),
+			array(array('1'),  TransformationFailedException::class),
+			array(array('0'),  TransformationFailedException::class),
+
+			array(new \stdClass(), TransformationFailedException::class),
+			array(new \WP_Query(), TransformationFailedException::class),
+
+			array(NULL,        0.0),
+		);
+
+		// Only for PHP 7
+		// this case throwing error but PHP 7 not catching it
+		/*$result = version_compare(phpversion(), '7');
+		if($result == 0 || $result == 1) {
+			$values[] = array(PHP_INT_MIN, TransformationFailedException::class);
+		}*/
+
+		$this->variants = $variants;
+	}
+}
--- tests/Options/Special/NumericOptionTest.php
+++ tests/Options/Special/NumericOptionTest.php
@@ -2,7 +2,7 @@
 namespace Korobochkin\WPKit\Tests\Options\Special;
 
 use Korobochkin\WPKit\Options\Special\NumericOption;
-use Symfony\Component\Form\Exception\TransformationFailedException;
+use Korobochkin\WPKit\Tests\DataSets\NumericOption\TypesTransformationSet;
 
 class NumericOptionTest extends \WP_UnitTestCase {
 
@@ -23,8 +23,9 @@ class NumericOptionTest extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * @dataProvider getDataCases
-	 * @var $value mixed Value to insert and test.
+	 * @dataProvider casesTypesAfterSaving
+	 *
+	 * @var $value    mixed Value to insert and test.
 	 * @var $expected mixed Value to compare output value with.
 	 */
 	public function testTypesAfterSaving($value, $expected) {
@@ -32,7 +33,7 @@ class NumericOptionTest extends \WP_UnitTestCase {
 			->set($value);
 
 		if(class_exists($expected)) {
-			if(method_exists($this, 'expectException')) {
+			if(PHP_VERSION_ID >= 70000) {
 				$this->expectException($expected);
 				$this->stub->flush();
 			} else {
@@ -49,13 +50,18 @@ class NumericOptionTest extends \WP_UnitTestCase {
 		}
 	}
 
+	public function casesTypesAfterSaving() {
+		return new TypesTransformationSet();
+	}
+
 	/**
-	 * @dataProvider getDataCases
-	 * @var $value mixed Value to insert and test.
+	 * @dataProvider casesTypesWithoutSaving
+	 *
+	 * @var $value    mixed Value to insert and test.
 	 * @var $expected mixed Value to compare output value with.
 	 */
 	public function testTypesWithoutSaving($value, $expected) {
-		$this->option->set($value);
+		$this->stub->set($value);
 
 		if(class_exists($expected)) {
 			$this->assertEquals($value, $this->stub->get());
@@ -64,55 +70,11 @@ class NumericOptionTest extends \WP_UnitTestCase {
 		}
 	}
 
-	public function testDefaultValue() {
-		$this->assertEquals(0.0, $this->stub->get());
+	public function casesTypesWithoutSaving() {
+		return new TypesTransformationSet();
 	}
 
-	public function getDataCases() {
-		$values = array(
-			array(true,        TransformationFailedException::class),
-			array(false,       TransformationFailedException::class),
-
-			array(1234,        1234.0),
-			array(0,           0.0),
-			array(-1234,       -1234.0),
-			//array(PHP_INT_MAX, TransformationFailedException::class), // this case throwing error but PHP 7 not catching it
-			//array(PHP_INT_MIN, true),
-
-			array(1.234,       1.234),
-			array(1.2e3,       1.2e3),
-			array(7E-10,       7E-10),
-			array(-1.234,      -1.234),
-			array(-1.2e3,      -1.2e3),
-			array(-7E-10,      -7E-10),
-
-			array('1',         1.0),
-			array('VALUE',     TransformationFailedException::class),
-			array('true',      TransformationFailedException::class),
-			array('false',     TransformationFailedException::class),
-			array('',          TransformationFailedException::class),
-			array('0',         0.0),
-
-			array(array(),     TransformationFailedException::class),
-			array(array(1),    TransformationFailedException::class),
-			array(array(1, 2), TransformationFailedException::class),
-			array(array(''),   TransformationFailedException::class),
-			array(array('1'),  TransformationFailedException::class),
-			array(array('0'),  TransformationFailedException::class),
-
-			array(new \stdClass(), TransformationFailedException::class),
-			array(new \WP_Query(), TransformationFailedException::class),
-
-			array(NULL,        0.0),
-		);
-
-		// Only for PHP 7
-		// this case throwing error but PHP 7 not catching it
-		/*$result = version_compare(phpversion(), '7');
-		if($result == 0 || $result == 1) {
-			$values[] = array(PHP_INT_MIN, TransformationFailedException::class);
-		}*/
-
-		return $values;
+	public function testDefaultValue() {
+		$this->assertEquals(0.0, $this->stub->get());
 	}
 }

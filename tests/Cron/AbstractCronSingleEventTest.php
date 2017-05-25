c056dd82ee053c47999901d2f7424e0b4f18d100
Kolya Korobochkin <me@korobochkin.com>
2017-05-25 23:39:56 +0300

    update tetst

--- tests/Cron/AbstractCronSingleEventTest.php
+++ tests/Cron/AbstractCronSingleEventTest.php
@@ -19,6 +19,9 @@ class AbstractCronSingleEventTest extends \WP_UnitTestCase {
 	}
 
 	public function testSchedule() {
+		$time = time();
+		$name = 'wp_kit_test_cron_event';
+
 		$this->stub->setTimestamp('123');
 
 		if(PHP_VERSION_ID >= 70000) {
@@ -35,7 +38,7 @@ class AbstractCronSingleEventTest extends \WP_UnitTestCase {
 			}
 		}
 
-		$this->stub->setTimestamp(time());
+		$this->stub->setTimestamp($time);
 
 		if(PHP_VERSION_ID >= 70000) {
 			// PHP 7
@@ -51,9 +54,14 @@ class AbstractCronSingleEventTest extends \WP_UnitTestCase {
 			}
 		}
 
-		$this->stub->setHook('__return_true');
+		$this->stub->setName($name);
 		$this->assertNull($this->stub->schedule());
+		$this->assertEquals($name, $this->stub->getName());
 
-
+		// And finally validate that this event added to WordPress
+		$tasks = _get_cron_array();
+		$this->assertTrue(isset($tasks[$time][$name]));
+		$this->assertNotEmpty($tasks[$time][$name]);
+		$this->assertEquals(1, count($tasks[$time][$name]));
 	}
 }

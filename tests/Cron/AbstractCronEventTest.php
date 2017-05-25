45a59d534338d2b6a9bf7284ccada5dd81ce61ff
Kolya Korobochkin <me@korobochkin.com>
2017-05-26 00:54:05 +0300

    testSchedule

--- tests/Cron/AbstractCronEventTest.php
+++ tests/Cron/AbstractCronEventTest.php
@@ -19,6 +19,76 @@ class AbstractCronEventTest extends \WP_UnitTestCase {
 	}
 
 	public function testSchedule() {
-		$this->markTestIncomplete();
+		$time = time() + HOUR_IN_SECONDS;
+		$name = 'wp_kit_test_cron_event';
+		$recurrence = 'hourly';
+		$tasks = _get_cron_array();
+
+		$this->assertFalse(isset($tasks[$time][$name]));
+
+		// Set wrong timestamp
+		$this->stub->setTimestamp('123');
+		$this->stub->setRecurrence('NOT_EXISTS');
+
+		if(PHP_VERSION_ID >= 70000) {
+			// PHP 7
+			$this->expectException(\LogicException::class);
+			$this->stub->schedule();
+		} else {
+			// PHP 5
+			try {
+				$this->stub->schedule();
+			}
+			catch(\Exception $exception) {
+				$this->assertTrue(is_a($exception, \LogicException::class));
+			}
+		}
+
+		$this->stub->setTimestamp($time);
+
+		if(PHP_VERSION_ID >= 70000) {
+			// PHP 7
+			$this->expectException(\LogicException::class);
+			$this->stub->schedule();
+		} else {
+			// PHP 5
+			try {
+				$this->stub->schedule();
+			}
+			catch(\Exception $exception) {
+				$this->assertTrue(is_a($exception, \LogicException::class));
+			}
+		}
+
+		$this->stub->setRecurrence($recurrence);
+
+		if(PHP_VERSION_ID >= 70000) {
+			// PHP 7
+			$this->expectException(\LogicException::class);
+			$this->stub->schedule();
+		} else {
+			// PHP 5
+			try {
+				$this->stub->schedule();
+			}
+			catch(\Exception $exception) {
+				$this->assertTrue(is_a($exception, \LogicException::class));
+			}
+		}
+
+		$this->stub->setName($name);
+		$this->assertNull($this->stub->schedule());
+		$this->assertEquals($name, $this->stub->getName());
+
+		// And finally validate that this event added to WordPress
+		$tasks = _get_cron_array();
+		//var_dump($tasks);
+		$this->assertTrue(isset($tasks[$time][$name]));
+		$this->assertNotEmpty($tasks[$time][$name]);
+		$this->assertEquals(1, count($tasks[$time][$name]));
+
+		reset($tasks);
+		$firstIndex = key($tasks[$time][$name]);
+		$this->assertEquals($recurrence, $tasks[$time][$name][$firstIndex]['schedule']);
 	}
 }

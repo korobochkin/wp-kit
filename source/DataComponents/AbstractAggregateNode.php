2b39b2c7d9a5314f59e95e22a1a8b3d4398466de
Kolya Korobochkin <me@korobochkin.com>
2017-06-10 19:20:57 +0300

    new draft stuff for aggregate nodes

---
+++ source/DataComponents/AbstractAggregateNode.php
@@ -0,0 +1,9 @@
+<?php
+namespace Korobochkin\WPKit\DataComponents;
+
+use Korobochkin\WPKit\DataComponents\Traits\AggregateNodeTrait;
+
+abstract class AbstractAggregateNode extends AbstractNode {
+
+	use AggregateNodeTrait;
+}

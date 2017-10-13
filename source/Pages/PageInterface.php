da19a583565c0dbcb05b13d812c41ac544baca4b
Kolya Korobochkin <me@korobochkin.com>
2017-10-14 02:36:02 +0300

    docs

--- source/Pages/PageInterface.php
+++ source/Pages/PageInterface.php
@@ -5,24 +5,75 @@ use Korobochkin\WPKit\Pages\Views\PageViewInterface;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Interface PageInterface represents single admin page in WordPress.
+ *
+ * Use this interface to create your page or use predefined classes which implements
+ * this interface.
+ */
 interface PageInterface
 {
+    /**
+     * Called to late construct the page instance.
+     *
+     * Usually defining priority of calling this method defined in register method.
+     *
+     * @see register
+     *
+     * @return $this For chain calls.
+     */
     public function lateConstruct();
 
+    /**
+     * Returns name of the page.
+     *
+     * @return string Name of the page.
+     */
     public function getName();
 
+    /**
+     * Setups name of the page.
+     *
+     * @param $name string Name of the page.
+     *
+     * @return $this For chain calls.
+     */
     public function setName($name);
 
     public function setParentPage(MenuPageInterface $page);
 
     public function getParentPage();
 
+    /**
+     * Returns the page title.
+     *
+     * @return string Title of the page.
+     */
     public function getPageTitle();
 
+    /**
+     * Setups the page title.
+     *
+     * @param $title string Title of the page.
+     *
+     * @return $this For chain calls.
+     */
     public function setPageTitle($title);
 
+    /**
+     * Returns the menu page title.
+     *
+     * Used as label (title) in WordPress aside menu.
+     *
+     * @return string Menu title of the page.
+     */
     public function getMenuTitle();
 
+    /**
+     * @param $title string Menu title of the page.
+     *
+     * @return $this For chain calls.
+     */
     public function setMenuTitle($title);
 
     public function getCapability();

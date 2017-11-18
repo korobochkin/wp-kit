ab9a405c3ec5bf36cc2e10c65563e8900e557171
Kolya Korobochkin <me@korobochkin.com>
2017-11-18 20:54:29 +0300

    spaces

--- source/AlmostControllers/AjaxStack.php
+++ source/AlmostControllers/AjaxStack.php
@@ -8,18 +8,18 @@ namespace Korobochkin\WPKit\AlmostControllers;
  */
 class AjaxStack extends Stack
 {
-	/**
-	 * @inheritdoc
-	 */
-	public function register()
-	{
-		if (empty($this->actions)) {
-			throw new \LogicException('You need set actions before call register method.');
-		}
+    /**
+     * @inheritdoc
+     */
+    public function register()
+    {
+        if (empty($this->actions)) {
+            throw new \LogicException('You need set actions before call register method.');
+        }
 
-		add_action('wp_ajax_'        . $this->actionName, array($this, 'handleRequest'));
-		add_action('wp_ajax_nopriv_' . $this->actionName, array($this, 'handleRequest'));
+        add_action('wp_ajax_'.$this->actionName, array($this, 'handleRequest'));
+        add_action('wp_ajax_nopriv_'.$this->actionName, array($this, 'handleRequest'));
 
-		return $this;
-	}
+        return $this;
+    }
 }

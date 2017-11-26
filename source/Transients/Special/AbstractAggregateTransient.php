25cc12d4111cc05b685fa0276d82a22fd6bec9d6
Kolya Korobochkin <me@korobochkin.com>
2017-11-26 19:11:55 +0300

    aggregate

---
+++ source/Transients/Special/AbstractAggregateTransient.php
@@ -0,0 +1,14 @@
+<?php
+namespace Korobochkin\WPKit\Transients\Special;
+
+use Korobochkin\WPKit\DataComponents\AggregateNodeInterface;
+use Korobochkin\WPKit\DataComponents\Traits\Aggregate\AggregateGetTrait;
+use Korobochkin\WPKit\Transients\AbstractTransient;
+
+/**
+ * Class AbstractAggregateTransient
+ */
+abstract class AbstractAggregateTransient extends AbstractTransient implements AggregateNodeInterface
+{
+    use AggregateGetTrait;
+}

810644d5fb68fc47eb4f15f81379f57214bb4db3
Kolya Korobochkin <me@korobochkin.com>
2017-11-26 20:52:20 +0300

    new tests

---
+++ tests/Options/Special/AbstractAggregateOptionTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Korobochkin\WPKit\Tests\Options\Special;
+
+use Korobochkin\WPKit\Options\Special\AbstractAggregateOption;
+use Korobochkin\WPKit\Tests\DataSets\AggregateDataSet;
+
+class AbstractAggregateOptionTest extends \WP_UnitTestCase
+{
+    /**
+     * @var AbstractAggregateOption
+     */
+    protected $stub;
+
+    /**
+     * Prepare option for tests.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->stub = $this->getMockForAbstractClass(AbstractAggregateOption::class);
+        $this->stub->setName('wp_kit_AbstractAggregateOption');
+    }
+
+    /**
+     * @dataProvider
+     *
+     * @param $defaultValue array
+     * @param $valueToSave array
+     * @param $valueToGet array
+     */
+    public function testGet($defaultValue, $valueToSave, $valueToGet)
+    {
+        $this->stub->setDefaultValue($defaultValue);
+        $this->assertEquals($defaultValue, $this->stub->get());
+
+        $this->stub->updateValue($valueToSave);
+        $this->assertNull($this->stub->getLocalValue());
+        $this->assertEquals($valueToGet, $this->stub->get());
+    }
+
+    public function casesGet()
+    {
+        return new AggregateDataSet();
+    }
+}

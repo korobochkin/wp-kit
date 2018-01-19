9863384170efb10364e06810b672f087fdeb26a7
Kolya Korobochkin <me@korobochkin.com>
2018-01-20 02:29:53 +0300

    yes

--- tests/DataSets/Runners/SecondRunner.php
+++ tests/DataSets/Runners/SecondRunner.php
@@ -3,18 +3,37 @@ namespace Korobochkin\WPKit\Tests\DataSets\Runners;
 
 use Korobochkin\WPKit\Runners\ContainerTrait;
 use Korobochkin\WPKit\Runners\RunnerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Class SecondRunner
  */
 class SecondRunner implements RunnerInterface
 {
-    use ContainerTrait;
-
     /**
      * @var ContainerInterface Container with services.
      */
     protected static $container;
+
+    /**
+     * Returns the ContainerBuilder with services.
+     *
+     * @return ContainerInterface Container with services.
+     */
+    public static function getContainer()
+    {
+        return self::$container;
+    }
+
+    /**
+     * Sets the ContainerBuilder with services.
+     *
+     * @param ContainerInterface $container Container with services.
+     */
+    public static function setContainer(ContainerInterface $container = null)
+    {
+        self::$container = $container;
+    }
 
     /**
      * @inheritdoc

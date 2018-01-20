9aff62e5e111ee43ac33f824968b118fd0d6d620
Kolya Korobochkin <me@korobochkin.com>
2018-01-20 23:23:39 +0300

    test again

--- tests/DataSets/Runners/SecondRunner.php
+++ tests/DataSets/Runners/SecondRunner.php
@@ -10,12 +10,30 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class SecondRunner implements RunnerInterface
 {
-    use ContainerTrait;
-
     /**
      * @var ContainerInterface Container with services.
      */
     protected static $container;
+
+    /**
+     * Returns the ContainerBuilder with services.
+     *
+     * @return ContainerInterface Container with services.
+     */
+    public static function getContainer()
+    {
+        return self::$container;
+    }
+
+    /**
+     * Sets the ContainerBuilder with services.
+     *
+     * @param ContainerInterface $container Container with services.
+     */
+    public static function setContainer(ContainerInterface $container = null)
+    {
+        self::$container = $container;
+    }
 
     /**
      * @inheritdoc

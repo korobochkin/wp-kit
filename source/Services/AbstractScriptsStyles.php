864c73602148917c7985452fe122b39c62323a0f
Kolya Korobochkin <me@korobochkin.com>
2018-01-22 02:11:15 +0300

    added scripts and styles service

---
+++ source/Services/AbstractScriptsStyles.php
@@ -0,0 +1,64 @@
+<?php
+namespace Korobochkin\WPKit\Services;
+
+/**
+ * Class AbstractScriptsStyles
+ */
+abstract class AbstractScriptsStyles implements ScriptsStylesInterface
+{
+    /**
+     * @var string Base url where to your assets folder.
+     */
+    protected $baseUrl;
+
+    /**
+     * @var bool Developer mode status. Used for enqueue non min files.
+     */
+    protected $dev;
+
+    /**
+     * ScriptsStyles constructor.
+     *
+     * @param string $baseUrl Base url to assets folder.
+     * @param bool $dev Status of developer mode.
+     */
+    public function __construct($baseUrl, $dev)
+    {
+        $this->baseUrl = $baseUrl;
+        $this->dev = $dev;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getBaseUrl()
+    {
+        return $this->baseUrl;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setBaseUrl($baseUrl)
+    {
+        $this->baseUrl = $baseUrl;
+        return $this;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function isDev()
+    {
+        return $this->dev;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setDev($dev)
+    {
+        $this->dev = $dev;
+        return $this;
+    }
+}
---
+++ source/Services/ScriptsStylesInterface.php
@@ -0,0 +1,47 @@
+<?php
+namespace Korobochkin\WPKit\Services;
+
+/**
+ * Interface ScriptsStylesInterface
+ */
+interface ScriptsStylesInterface
+{
+    /**
+     * Register all required scripts and styles in WordPress.
+     *
+     * @return $this For chain calls.
+     */
+    public function register();
+
+    /**
+     * Returns base url.
+     *
+     * @return string Base url to assets folder.
+     */
+    public function getBaseUrl();
+
+    /**
+     * Sets base url.
+     *
+     * @param string $baseUrl Base url to assets folder.
+     *
+     * @return $this For chain calls.
+     */
+    public function setBaseUrl($baseUrl);
+
+    /**
+     * Returns status of developer mode.
+     *
+     * @return bool Status of developer mode.
+     */
+    public function isDev();
+
+    /**
+     * Sets status of developer mode.
+     *
+     * @param bool $dev Status of developer mode.
+     *
+     * @return $this For chain calls.
+     */
+    public function setDev($dev);
+}

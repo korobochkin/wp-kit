a26d9d9a2c69d80e768ed4e086fd8117b846730d
Kolya Korobochkin <me@korobochkin.com>
2018-02-17 01:19:26 +0300

    added get+set + tests

---
+++ tests/Services/Translations/PluginTranslationsTest.php
@@ -0,0 +1,15 @@
+<?php
+namespace Korobochkin\WPKit\Tests\Services\Translations;
+
+use Korobochkin\WPKit\Services\Translations\PluginTranslations;
+
+class PluginTranslationsTest extends \WP_UnitTestCase
+{
+    public function testLoadTranslations()
+    {
+        $path = WP_PLUGIN_DIR . '/test-plugin-domain/';
+        $stub = new PluginTranslations('test-plugin-domain', $path);
+
+        $this->assertEquals($stub, $stub->loadTranslations());
+    }
+}

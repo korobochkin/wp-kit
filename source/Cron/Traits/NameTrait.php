7b75f2e2b0d949e374a0880fb796c9ce6dc1c8dc
Kolya Korobochkin <me@korobochkin.com>
2018-02-18 17:46:16 +0300

    doc

--- source/Cron/Traits/NameTrait.php
+++ source/Cron/Traits/NameTrait.php
@@ -8,15 +8,27 @@ namespace Korobochkin\WPKit\Cron\Traits;
 trait NameTrait
 {
     /**
-     * @var string
+     * @var string Name of event
      */
     protected $name;
 
+    /**
+     * Returns name of event.
+     *
+     * @return string Name of event
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * Sets name of event.
+     *
+     * @param $name string Name of event.
+     *
+     * @return $this For chain calls.
+     */
     public function setName($name)
     {
         $this->name = $name;

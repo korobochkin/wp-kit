aecc0e7683b2a81a8f80c97dc9f453150f3cb3a1
Kolya Korobochkin <me@korobochkin.com>
2018-02-25 01:09:21 +0300

    align

--- tests/Notices/NoticeErrorViewTest.php
+++ tests/Notices/NoticeErrorViewTest.php
@@ -3,12 +3,15 @@ namespace Korobochkin\WPKit\Tests\Notices;
 
 use Korobochkin\WPKit\Notices\NoticeErrorView;
 
+/**
+ * Class NoticeErrorViewTest
+ */
 class NoticeErrorViewTest extends \WP_UnitTestCase
 {
     public function testCssClasses()
     {
         $stub = new NoticeErrorView();
-        $rp = new \ReflectionProperty(NoticeErrorView::class, 'cssClasses');
+        $rp   = new \ReflectionProperty(NoticeErrorView::class, 'cssClasses');
         $rp->setAccessible(true);
         $this->assertEquals(array('notice', 'notice-error'), $rp->getValue($stub));
     }

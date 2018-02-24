75c16caf7eb13bbe5be1e77b7bc3fdec0c446de6
Kolya Korobochkin <me@korobochkin.com>
2018-02-25 02:40:19 +0300

    testssss

---
+++ tests/Notices/NoticeSuccessViewTest.php
@@ -0,0 +1,26 @@
+<?php
+namespace Korobochkin\WPKit\Tests\Notices;
+
+use Korobochkin\WPKit\Notices\NoticeSuccessView;
+
+/**
+ * Class NoticeSuccessViewTest
+ */
+class NoticeSuccessViewTest extends \WP_UnitTestCase
+{
+    public function testCssClasses()
+    {
+        $stub = new NoticeSuccessView();
+        $rp = new \ReflectionProperty(NoticeSuccessView::class, 'cssClasses');
+        $rp->setAccessible(true);
+        $this->assertEquals(
+            array(
+                'notice',
+                'notice-success',
+                'wp-kit-notice',
+                'wp-kit-notice-success'
+            ),
+            $rp->getValue($stub)
+        );
+    }
+}

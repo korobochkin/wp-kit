1a8aad08d04f59c490e55e2c820aff4d628a55d2
Kolya Korobochkin <me@korobochkin.com>
2018-03-04 00:00:55 +0300

    Uninstall

--- source/Uninstall/Uninstall.php
+++ source/Uninstall/Uninstall.php
@@ -1,5 +1,5 @@
 <?php
-namespace Korobochkin\WPKit\Services\Uninstall;
+namespace Korobochkin\WPKit\Uninstall;
 
 use Korobochkin\WPKit\Cron\CronEventInterface;
 use Korobochkin\WPKit\Options\OptionInterface;

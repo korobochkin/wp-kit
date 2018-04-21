354bb91685527448bdbfe64c854a1f6b28565b56
Kolya Korobochkin <me@korobochkin.com>
2018-04-21 23:48:52 +0300

    fixed and extended AbstractTransientTest

--- tests/Transients/AbstractTransientTest.php
+++ tests/Transients/AbstractTransientTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Korobochkin\WPKit\Tests\Transients;
 
+use Korobochkin\WPKit\Tests\DataSets\EverythingSet2;
 use Korobochkin\WPKit\Transients\Transient;
 
 /**
@@ -30,153 +31,306 @@ class AbstractTransientTest extends \WP_UnitTestCase
      */
     public function testGetValueFromWordPress()
     {
-        if (method_exists($this, 'expectException')) {
-            // PHP 7.
+        if (PHP_VERSION_ID >= 70000) {
             $this->expectException(\LogicException::class);
             $this->stub->getValueFromWordPress();
         } else {
-            // PHP 5.
             try {
                 $this->stub->getValueFromWordPress();
             } catch (\Exception $exception) {
-                $this->assertTrue(is_a($exception, \LogicException::class));
+                $this->assertInstanceOf(\LogicException::class, $exception);
+            } finally {
+                $this->assertInstanceOf(\LogicException::class, $exception);
             }
         }
-    }
 
-    /**
-     * Test with setting name before request value.
-     */
-    public function testGetValueFromWordPressWithName()
-    {
-        $this->stub->setName('wp_kit_abstract_trait');
+        $this->stub->setName('wp_kit_abstract_option');
         $this->assertFalse($this->stub->getValueFromWordPress());
     }
 
-    public function testDeleteFromWP()
+    /**
+     * Test deleting value in WordPress.
+     *
+     * @dataProvider casesDeleteFromWP
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testDeleteFromWP($value, $saveResult, $valueResult, $deleteResult)
     {
-        if (method_exists($this, 'expectException')) {
-            // PHP 7.
+        if (PHP_VERSION_ID >= 70000) {
             $this->expectException(\LogicException::class);
             $this->stub->deleteFromWP();
         } else {
-            // PHP 5.
             try {
                 $this->stub->deleteFromWP();
             } catch (\Exception $exception) {
-                $this->assertTrue(is_a($exception, \LogicException::class));
+                $this->assertInstanceOf(\LogicException::class, $exception);
+            } finally {
+                $this->assertInstanceOf(\LogicException::class, $exception);
             }
         }
 
-        $this->stub->setName('wp_kit_abstract_trait');
-        $this->stub->updateValue('hello');
-
-        // Check that successful pushed into DB.
-        $this->assertSame('hello', $this->stub->getValueFromWordPress());
-
-        // Check that successful remove from DB.
-        $this->assertTrue($this->stub->deleteFromWP());
-        $this->assertSame(false, $this->stub->getValueFromWordPress());
-    }
-
-    /**
-     * @dataProvider flushCases
-     */
-    public function testFlush($value, $expected)
-    {
-        $this->stub->setName('wp_kit_abstract_trait');
-        $this->assertSame($expected, $this->stub->set($value)->flush());
-    }
-
-    public function flushCases()
-    {
-        return array(
-            array(true, true),
-            array(false, true),
-
-            array(1234, true),
-            array(0, true),
-            array(-1234, true),
-            array(PHP_INT_MAX, true),
-            //array(PHP_INT_MIN, true),
-
-            array(1.234, true),
-            array(1.2e3, true),
-            array(7E-10, true),
-            array(-1.234, true),
-            array(-1.2e3, true),
-            array(-7E-10, true),
-
-            array('1', true),
-            array('VALUE', true),
-            array('true', true),
-            array('false', true),
-            array('', true),
-            array('0', true),
-
-            array(array(), true),
-            array(array(1), true),
-            array(array(1, 2), true),
-            array(array(''), true),
-            array(array('1'), true),
-            array(array('0'), true),
-
-            array(new \stdClass(), true),
-            array(new \WP_Query(), true),
-
-            array(null, true),
-        );
-    }
-
-    /**
-     * @dataProvider flushValueCases
-     */
-    public function testValueAfterSaving($value, $expected)
-    {
         $this->stub
             ->setName('wp_kit_abstract_trait')
-            ->set($value)
-            ->flush();
-        $this->assertSame($expected, $this->stub->get());
+            ->updateValue($value);
+
+        $this->assertSame($deleteResult, $this->stub->deleteFromWP());
+        $this->assertFalse($this->stub->getValueFromWordPress());
     }
 
-    public function flushValueCases()
+    public function casesDeleteFromWP()
     {
-        return array(
-            array(true, '1'),
-            array(false, false),
+        return new EverythingSet2();
+    }
 
-            array(1234, '1234'),
-            array(0, '0'),
-            array(-1234, '-1234'),
-            array(PHP_INT_MAX, (string) PHP_INT_MAX),
-            //array(PHP_INT_MIN, true),
+    /**
+     * Test flushing (saving) values into WordPress with flush().
+     *
+     * @dataProvider casesFlush
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testFlush($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->stub->set($value);
 
-            array(1.234, '1.234'),
-            array(1.2e3, '1.2e3'),
-            array(7E-10, '7E-10'),
-            array(-1.234, '-1.234'),
-            array(-1.2e3, '-1.2e3'),
-            array(-7E-10, '-7E-10'),
+        // Catch \LogicException if name was not specified.
+        if (PHP_VERSION_ID >= 70000) {
+            $this->expectException(\LogicException::class);
+            $this->stub->flush();
+        } else {
+            try {
+                $this->stub->flush();
+            } catch (\Exception $exception) {
+                $this->assertInstanceOf(\LogicException::class, $exception);
+            } finally {
+                $this->assertInstanceOf(\LogicException::class, $exception);
+            }
+        }
 
-            array('1', '1'),
-            array('VALUE', 'VALUE'),
-            array('true', 'true'),
-            array('false', 'false'),
-            array('', ''),
-            array('0', '0'),
+        if (null !== $value) {
+            $this->assertSame($value, $this->stub->get());
+        }
 
-            array(array(), array()),
-            array(array(1), array(1)),
-            array(array(1, 2), array(1, 2)),
-            array(array(''), array('')),
-            array(array('1'), array('1')),
-            array(array('0'), array('0')),
+        $this->stub->setName('wp_kit_abstract_option');
 
-            array(new \stdClass(), new \stdClass()),
-            array(new \WP_Query(), new \WP_Query()),
+        $this->assertSame($saveResult, $this->stub->flush());
 
-            array(null, ''),
-        );
+        wp_cache_flush();
+
+        if (true === $saveResult) {
+            if (is_object($value)) {
+                $this->assertEquals($valueResult, $this->stub->getValueFromWordPress());
+            } else {
+                $this->assertSame($valueResult, $this->stub->getValueFromWordPress());
+            }
+            $this->assertSame(null, $this->stub->getLocalValue());
+        } else {
+            $this->assertSame(false, $this->stub->getValueFromWordPress());
+            $this->assertSame($value, $this->stub->getLocalValue());
+        }
+    }
+
+    public function casesFlush()
+    {
+        return new EverythingSet2();
+    }
+
+    /**
+     * Testing flushing (saving) values into WordPress with updateValue().
+     *
+     * @dataProvider casesUpdateValue
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testUpdateValue($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->stub
+            ->setName('wp_kit_abstract_transient');
+
+        $this->assertSame($saveResult, $this->stub->updateValue($value));
+        wp_cache_flush();
+
+        if (true === $saveResult) {
+            if (is_object($value)) {
+                $this->assertEquals($valueResult, $this->stub->getValueFromWordPress());
+            } else {
+                $this->assertSame($valueResult, $this->stub->getValueFromWordPress());
+            }
+            $this->assertSame(null, $this->stub->getLocalValue());
+        } else {
+            $this->assertSame(false, $this->stub->getValueFromWordPress());
+            $this->assertSame($value, $this->stub->getLocalValue());
+        }
+    }
+
+    public function casesUpdateValue()
+    {
+        return new EverythingSet2();
+    }
+
+    /**
+     * Testing get() method.
+     *
+     * @dataProvider casesGet
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testGet($value, $saveResult, $valueResult, $deleteResult)
+    {
+        // Set name to prevent triggering exceptions.
+        $this->stub->setName('wp_kit_abstract_transient');
+
+        // Test that local value returned.
+        $this->stub->setLocalValue($value);
+        $this->assertSame($value, $this->stub->get());
+
+        // Reset local value.
+        $this->stub->setLocalValue(null);
+
+        // Check default value.
+        $this->assertSame(null, $this->stub->get());
+
+        // Check Default value again.
+        $this->stub->setDefaultValue($value);
+        $this->assertSame($value, $this->stub->get());
+
+        // Check returning local value.
+        $this->stub->setDefaultValue(uniqid('wp_kit', true));
+        $this->stub->setLocalValue($value);
+        if ($value === null) {
+            $this->assertSame($this->stub->getDefaultValue(), $this->stub->get());
+        } else {
+            $this->assertSame($value, $this->stub->get());
+        }
+
+        // Check value from WordPress after saving.
+        $this->stub->flush();
+        wp_cache_flush();
+        if (true === $saveResult) {
+            if (is_object($value)) {
+                $this->assertEquals($valueResult, $this->stub->get());
+            } else {
+                $this->assertSame($valueResult, $this->stub->get());
+            }
+        } else {
+            $this->assertSame($value, $this->stub->get());
+            $this->assertSame($value, $this->stub->getLocalValue());
+        }
+    }
+
+    public function casesGet()
+    {
+        return new EverythingSet2();
+    }
+
+    /**
+     * Testing set() method.
+     *
+     * @dataProvider casesSet
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testSet($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->stub->setName('wp_kit_abstract_option');
+
+        $this->assertSame($this->stub, $this->stub->set($value));
+        $this->assertSame($value, $this->stub->get());
+        $this->assertSame($value, $this->stub->getLocalValue());
+    }
+
+    public function casesSet()
+    {
+        return new EverythingSet2();
+    }
+
+    public function testName()
+    {
+        $this->assertNull($this->stub->getName());
+
+        $this->assertSame($this->stub, $this->stub->setName('wp_kit_dummy_name'));
+        $this->assertSame('wp_kit_dummy_name', $this->stub->getName());
+    }
+
+    /**
+     * Testing setLocalValue() and getLocalValue() methods.
+     *
+     * @dataProvider casesLocalValue
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testLocalValue($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->assertNull($this->stub->getLocalValue());
+        $this->assertSame($this->stub, $this->stub->setLocalValue($value));
+        $this->assertSame($value, $this->stub->getLocalValue());
+    }
+
+    public function casesLocalValue()
+    {
+        return new EverythingSet2();
+    }
+
+    /**
+     * Testing Getter and Setter for default value.
+     *
+     * @dataProvider casesDefaultValue
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testDefaultValue($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->assertNull($this->stub->getDefaultValue());
+        $this->assertSame($this->stub, $this->stub->setDefaultValue($value));
+        $this->assertSame($value, $this->stub->getDefaultValue());
+    }
+
+    public function casesDefaultValue()
+    {
+        return new EverythingSet2();
+    }
+
+    /**
+     * Test deleting local value.
+     *
+     * @dataProvider casesDeleteLocalValue
+     *
+     * @param $value mixed Any variable types.
+     * @param $saveResult bool Result of saving $value in WordPress.
+     * @param $valueResult mixed $value returned by WordPress.
+     * @param $deleteResult bool Result of deleting $value in WordPress.
+     */
+    public function testDeleteLocalValue($value, $saveResult, $valueResult, $deleteResult)
+    {
+        $this->assertSame($this->stub, $this->stub->setLocalValue($value));
+        $this->assertTrue($this->stub->deleteLocal());
+        $this->assertNull($this->stub->getLocalValue());
+    }
+
+    public function casesDeleteLocalValue()
+    {
+        return new EverythingSet2();
     }
 }

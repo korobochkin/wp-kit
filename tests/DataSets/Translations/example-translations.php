26503f071d6e5df101bc9e87bcd558fbe76f0dc5
Kolya Korobochkin <me@korobochkin.com>
2018-10-27 20:05:07 +0300

    mu plugin translations

---
+++ tests/DataSets/Translations/example-translations.php
@@ -0,0 +1,3 @@
+<?php
+__('Hi', 'wp-kit-example');
+_n('Comment', 'Comments', 2, 'wp-kit-example');
---
+++ tests/Translations/MUPluginTranslationsTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Korobochkin\WPKit\Tests\Translations;
+
+use Korobochkin\WPKit\Tests\DataSets\EverythingSet;
+use Korobochkin\WPKit\Translations\MUPluginTranslations;
+
+class MUPluginTranslationsTest extends \WP_UnitTestCase
+{
+    /**
+     * @inheritdoc
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        add_filter('locale', array($this, 'filterLocale'));
+    }
+
+    /**
+     * @param string $locale The locale ID.
+     * @return string Modified locale.
+     */
+    public function filterLocale($locale)
+    {
+        return 'ru_RU';
+    }
+
+    public function testLoadTranslations()
+    {
+        $path = WPMU_PLUGIN_DIR . '/test-plugin/';
+        $stub = new MUPluginTranslations('wp-kit-example', $path);
+
+        $this->assertSame($stub, $stub->loadTranslations());
+    }
+
+    public function testTranslationsWorks()
+    {
+        $reflection  = new \ReflectionClass(EverythingSet::class);
+        $source      = dirname($reflection->getFileName()) . '/Translations/wp-kit-example-ru_RU.mo';
+        $destination = WPMU_PLUGIN_DIR . '/wp-kit-example/translations/';
+        copy($source, $destination);
+
+        $stub = new MUPluginTranslations('wp-kit-example', $destination);
+        $stub->loadTranslations();
+
+        $this->assertSame('Привет', __('Hi', 'wp-kit-example'));
+        $this->assertSame('Коментарий', _n('Comment', 'Comments', 1, 'wp-kit-example'));
+        $this->assertSame('Коментария', _n('Comment', 'Comments', 2, 'wp-kit-example'));
+        $this->assertSame('Коментариев', _n('Comment', 'Comments', 5, 'wp-kit-example'));
+    }
+}

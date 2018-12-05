9813e088a1251330d26b49d28a8682fc04405fb3
Kolya Korobochkin <me@korobochkin.com>
2018-12-06 01:27:11 +0300

    added test for trait

---
+++ tests/DataComponents/Traits/Special/Bool/BoolBuildConstraintTraitTest.php
@@ -0,0 +1,27 @@
+<?php
+namespace Korobochkin\WPKit\Tests\DataComponents\Traits\Special\Bool;
+
+use Korobochkin\WPKit\DataComponents\Traits\Special\Bool\BoolBuildConstraintTrait;
+use Symfony\Component\Validator\Constraints;
+
+class BoolBuildConstraintTraitTest extends \WP_UnitTestCase
+{
+    public function testBuildConstraint()
+    {
+        /**
+         * @var $stub BoolBuildConstraintTrait
+         */
+        $stub = $this->getMockForTrait(BoolBuildConstraintTrait::class);
+        $this->assertEquals(
+            array(
+                new Constraints\NotNull(),
+                new Constraints\Type(
+                    array(
+                        'type' => 'bool',
+                    )
+                ),
+            ),
+            $stub->buildConstraint()
+        );
+    }
+}

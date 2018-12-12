549a21ff461f45549f43f9a6c2ed8a499a3de181
Kolya Korobochkin <me@korobochkin.com>
2018-12-12 22:02:47 +0300

    added tests for MetaBox

---
+++ tests/MetaBoxes/MetaBoxTestingPurposesView.php
@@ -0,0 +1,16 @@
+<?php
+namespace Korobochkin\WPKit\Tests\MetaBoxes;
+
+use Korobochkin\WPKit\MetaBoxes\MetaBoxInterface;
+use Korobochkin\WPKit\MetaBoxes\MetaBoxViewInterface;
+
+class MetaBoxTestingPurposesView implements MetaBoxViewInterface
+{
+    /**
+     * @param MetaBoxInterface $metaBox
+     */
+    public function render(MetaBoxInterface $metaBox)
+    {
+        echo '<div class="wp-kit-test-meta-box">This is test Meta Box view instance. Title of Meta Box: <code>' . $metaBox->getTitle() . '</code></div>';
+    }
+}

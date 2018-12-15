616a63ee2b2b3ce9da1130eed67bce764a49a6dc
Kolya Korobochkin <me@korobochkin.com>
2018-12-15 22:41:30 +0300

    AbstractScriptsStylesTest

---
+++ tests/ScriptsStyles/AbstractScriptsStylesTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Korobochkin\WPKit\Tests\ScriptsStyles;
+
+use Korobochkin\WPKit\ScriptsStyles\AbstractScriptsStyles;
+
+class AbstractScriptsStylesTest extends \WP_UnitTestCase
+{
+    /**
+     * @var AbstractScriptsStyles
+     */
+    protected $stub;
+
+    function setUp()
+    {
+        parent::setUp();
+
+        $this->stub = $this->getMockForAbstractClass(
+            AbstractScriptsStyles::class,
+            array(
+                null,
+                null,
+            )
+        );
+    }
+
+    public function testConstruct()
+    {
+        /**
+         * @var $stub AbstractScriptsStyles
+         */
+        $stub = $this->getMockForAbstractClass(
+            AbstractScriptsStyles::class,
+            array(
+                'http://example.org/wp-content/plugins/wp-kit-based-plugin/',
+                false,
+            )
+        );
+
+        $this->assertSame(
+            'http://example.org/wp-content/plugins/wp-kit-based-plugin/',
+            $stub->getBaseUrl()
+        );
+        $this->assertFalse($stub->isDev());
+    }
+
+    public function testGetterAndSetterBaseUrl()
+    {
+        $value = 'http://example.org/wp-content/plugins/wp-kit-based-plugin/';
+        $this->assertNull($this->stub->getBaseUrl());
+        $this->assertSame($this->stub, $this->stub->setBaseUrl($value));
+        $this->assertSame($value, $this->stub->getBaseUrl());
+    }
+
+    public function testGetterAndSetterDev()
+    {
+        $value = true;
+        $this->assertNull($this->stub->isDev());
+        $this->assertSame($this->stub, $this->stub->setDev($value));
+        $this->assertSame($value, $this->stub->isDev());
+    }
+}

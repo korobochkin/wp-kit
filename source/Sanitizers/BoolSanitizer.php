7977d016c109bd516ecd861b812ca440876fb4c6
Kolya Korobochkin <me@korobochkin.com>
2018-12-16 22:27:02 +0300

    added tests for bool sanitizer

--- source/Sanitizers/BoolSanitizer.php
+++ source/Sanitizers/BoolSanitizer.php
@@ -9,14 +9,6 @@ class BoolSanitizer implements SanitizerInterface
 {
     public static function sanitize($value)
     {
-        if (is_string($value)) {
-            if ($value == '1') {
-                return true;
-            } elseif ($value == '0') {
-                return false;
-            }
-        }
-
         return (bool) $value;
     }
 }

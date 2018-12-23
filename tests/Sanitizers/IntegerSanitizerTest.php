fb8484585a5479e2e68149c6e750c9168a56a660
Kolya Korobochkin <me@korobochkin.com>
2018-12-23 14:13:48 +0300

    int sanitizer tests

---
+++ tests/Sanitizers/IntegerSanitizerTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Korobochkin\WPKit\Tests\Sanitizers;
+
+use Korobochkin\WPKit\Sanitizers\IntegerSanitizer;
+use Korobochkin\WPKit\Tests\DataSets\IntegerSanitizeSet;
+
+class IntegerSanitizerTest extends \WP_UnitTestCase
+{
+    /**
+     * @dataProvider casesSanitize
+     * @param $value mixed Input value.
+     * @param $expected int Result of sanitizing.
+     */
+    public function testSanitize($value, $expected)
+    {
+        $this->assertSame($expected, IntegerSanitizer::sanitize($value));
+    }
+
+    public function casesSanitize()
+    {
+        return new IntegerSanitizeSet();
+    }
+}

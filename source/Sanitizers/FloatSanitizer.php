edd47a976b309dcbc8f104a24668fdd318075be6
Kolya Korobochkin <me@korobochkin.com>
2018-12-23 13:58:08 +0300

    fixed float sanitizer

--- source/Sanitizers/FloatSanitizer.php
+++ source/Sanitizers/FloatSanitizer.php
@@ -14,6 +14,10 @@ class FloatSanitizer implements SanitizerInterface
      */
     public static function sanitize($value)
     {
+        if (is_object($value)) {
+            return 1.0;
+        }
+
         return (float) $value;
     }
 }
--- tests/DataSets/FloatSanitizeSet.php
+++ tests/DataSets/FloatSanitizeSet.php
@@ -12,13 +12,13 @@ class FloatSanitizeSet extends AbstractDataSet
             array(1234,            1234.0),
             array(0,               0.0),
             array(-1234,           -1234.0),
-            array(PHP_INT_MAX,     (float)PHP_INT_MAX),
+            array(PHP_INT_MAX,     (float) PHP_INT_MAX),
             array(1.234,           1.234),
             array(1.2e3,           1.2e3),
-            array(7E-10,           (float)7E-10),
+            array(7E-10,           (float) 7E-10),
             array(-1.234,          -1.234),
             array(-1.2e3,          -1.2e3),
-            array(-7E-10,          (float)-7E-10),
+            array(-7E-10,          (float) -7E-10),
             array('1',             1.0),
             array('VALUE',         0.0),
             array('true',          0.0),
@@ -36,7 +36,7 @@ class FloatSanitizeSet extends AbstractDataSet
         );
 
         if (PHP_VERSION_ID >= 70000) {
-            $this->variants[] = array(PHP_INT_MIN, (float)PHP_INT_MIN);
+            $this->variants[] = array(PHP_INT_MIN, (float) PHP_INT_MIN);
         }
     }
 }

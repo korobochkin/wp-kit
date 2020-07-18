bfb73686fa4061e247c0d01e53883c0e892ade49
Kolya Korobochkin <korobochkin@users.noreply.github.com>
2020-07-18 23:22:58 +0300

    php unit 5.6 for WordPress 4.0

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -2,8 +2,13 @@
 
 bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION;
 
+if [[ ${WP_VERSION} == '4.0' ]]; then
+    composer require phpunit/phpunit=5.6;
+else
+    composer require phpunit/phpunit:^7;
+fi
+
 composer require \
-    phpunit/phpunit:^6 \
     squizlabs/php_codesniffer \
 ;
 

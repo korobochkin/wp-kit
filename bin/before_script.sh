62daf43c3f35ac87875e2cd610e8365340ad6b85
Kolya Korobochkin <me@korobochkin.com>
2020-07-18 21:20:04 +0300

    fixed before_script.sh require correct version of phpunit on new php versions remove squizlabs/php_codesniffer from composer deps (install as phpunit from bash script)

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -2,17 +2,15 @@
 
 bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION;
 
-echo 'ENV VARIABLE WP_VERSION IS: ';
-echo "$WP_VERSION";
+composer require \
+    phpunit/phpunit:^7 \
+    squizlabs/php_codesniffer \
+;
 
-if [[ ${WP_VERSION} == '4.0' ]]; then
-    composer require "phpunit/phpunit=5.6.*";
-else
-   if [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
-        composer require phpunit/phpunit;
-    else
-        composer require "phpunit/phpunit=5.6.*";
-    fi
-fi
-
-composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader --classmap-authoritative;
+composer install \
+    --no-ansi \
+    --no-interaction \
+    --no-progress \
+    --optimize-autoloader \
+    --classmap-authoritative \
+;

229a0458512bd57a456d6238abff2e998f568d36
Kolya Korobochkin <korobochkin@users.noreply.github.com>
2020-07-18 21:50:18 +0300

    phpunit/phpunit:^6

--- bin/before_script.sh
+++ bin/before_script.sh
@@ -3,7 +3,7 @@
 bash bin/install-wp-tests.sh wordpress_test root '' localhost $WP_VERSION;
 
 composer require \
-    phpunit/phpunit:^7 \
+    phpunit/phpunit:^6 \
     squizlabs/php_codesniffer \
 ;
 
